7958b5427d6ce58ef3524f9872158df71d0bd2c1
samikayaa <samikaya89@gmail.com>
2025-05-07 22:47:32 +0300

    c

--- sprint6/gun2/antremanlar.js
+++ sprint6/gun2/antremanlar.js
@@ -8,20 +8,6 @@ const MyComponent = (props) => {
 
 
 
-// caseX
-// kimden import kimden export ediyorum?
-
-//utils.js 'im var
-export default function getUser() {
-    return {name: "Sami", age: 30};
-}
-
-
-// index.js 'im var
-import getUser from '/utils.js'
-
-console.log(getUser());
-
 
 
   
