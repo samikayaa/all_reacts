898a345ad5349e0c0b71e0f09465a41c1b29f66c
samikayaa <samikaya89@gmail.com>
2025-05-11 15:19:53 +0300

    c

--- sprint6/gun2/case6.js
+++ sprint6/gun2/case6.js
@@ -1,4 +1,26 @@
-//Aşağıdaki kodda App component’i içerisinde handleClick fonksiyonunu yazarak, prop olarak Button component'ine yolla ve butona tıklandığında konsola Merhaba, React! yazdırılmasını sağla.
+//Aşağıdaki kodda App component’i içerisinde handleClick fonksiyonunu yazarak, 
+//prop olarak Button component'ine yolla ve butona tıklandığında konsola "Merhaba, React!" yazdırılmasını sağla.
+
+
+import Button from './Button.js';
+
+export default function App() {
+
+  function handleClick() {
+    console.log("Merhaba, React!");
+  }
+
+  return <Button onClick={handleClick}/>; 
+}
+
+///////////////////////////////////////////////////////
+
+import App from './App.js';
+
+const Button = (props) => { 
+  const {onClick} = props;
+	return <button onClick={onClick}>Tıkla</button>;
+ }; 
 
 /*
 App.js
