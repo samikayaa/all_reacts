a7feb46ee85fe5cebccb179ad683b65b728fecb8
samikayaa <samikaya89@gmail.com>
2025-05-14 20:26:53 +0300

    c

--- sprint6/gun4/case1.jsx
+++ sprint6/gun4/case1.jsx
@@ -1,1 +1,19 @@
-//
+//div#heading1 element'ine inline stil uygula. 
+// - Arkaplan rengi Kırmızı olsun. -20px'lik bir padding olsun.
+
+/*
+
+export default function App(){
+ return (
+   <div id="heading1">Merhaba Dünya</div>
+ )
+}
+
+*/
+
+export default function App(){
+    return (
+      <div id="heading1" style={{backgroundColor:"red", padding=20px}}>Merhaba Dünya</div>
+    )
+   }
+   
