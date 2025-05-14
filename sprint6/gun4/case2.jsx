effa960f981b26a8c1f412ac0457e2ded6c5b910
samikayaa <samikaya89@gmail.com>
2025-05-14 20:37:01 +0300

    c

---
+++ sprint6/gun4/case2.jsx
@@ -0,0 +1,44 @@
+//styled-component ile hazırlanmış Button'u App component'inde form içinde kullan. 
+
+//- Butonda Kaydet yazsın. 
+//- Arkaplan rengi yeşil olsun.
+
+import styled from 'styled-components';
+
+const Button = styled.button`
+ background-color: ${props => props.bgColor || "blue"}
+`
+
+export default function App(){
+ return (
+   <form id="newsletter">
+     <input name="email" />
+     <Button bgColor="green">Kaydet</Button>
+   </form>
+ )
+}
+
+
+
+
+
+
+/*
+
+SORU:
+
+import styled from 'styled-components';
+
+const Button = styled.button`
+ background-color: ${props => props.bgColor || "blue"}
+`
+
+export default function App(){
+ return (
+   <form id="newsletter">
+     <input name="email" />
+   </form>
+ )
+}
+
+*/
