9d304db0673252df58f452d83fae2f12b7ae23a8
samikayaa <samikaya89@gmail.com>
2025-05-19 12:12:32 +0300

    c

--- sprint7/gun1/case1.jsx
+++ sprint7/gun1/case1.jsx
@@ -1,1 +1,95 @@
-a
+//Aşağıdaki uygulamada, iki farklı sayfa için routing yap: 
+
+//Ana Sayfa ("/") ve Hakkımızda Sayfası ("/hakkimizda"). `BrowserRouter`, `Switch` ve `Route` bileşenlerini kullanarak bu route'ları tanımla. 
+
+//- `HomePage` ve `AboutPage` component'leri tanımlanmıştır. 
+//- Gerekli yerde exact prop'unu kullanmayı unutma.
+
+
+
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+function HomePage() {
+ return <div>Ana Sayfa</div>;
+}
+
+
+function AboutPage() {
+ return <div>Hakkımızda</div>;
+}
+
+
+function App() {
+ return (
+   <Router>
+     <Switch>
+        <Route path="/" exact>
+            <HomePage/>
+        </Route>
+        <Route path="/hakkimizda">
+            <AboutPage/>
+        </Route>
+
+     </Switch>
+   </Router>
+ );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+function HomePage() {
+ return <div>Ana Sayfa</div>;
+}
+
+
+function AboutPage() {
+ return <div>Hakkımızda</div>;
+}
+
+
+function App() {
+ return (
+   <Router>
+     <Switch>
+
+
+     </Switch>
+   </Router>
+ );
+}
+
+
+export default App;
+
+*/
---
+++ sprint7/gun1/case2.jsx
@@ -0,0 +1,88 @@
+//Aşağıdaki uygulamada, bir eğitim platformunun ana navigasyon menüsünü oluştur. 
+
+//- "Dersler" ve "Profil" sayfalarına gitmek için <Link> component'ini kullan. 
+
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
+import Dersler from './Dersler';
+import Profil from './Profil';
+
+
+function App() {
+ return (
+   <Router>
+     <nav>
+        <link to="/dersler">
+            <Dersler />
+        </link>
+        <link to="/profil">
+            <Profil />
+        </link>
+     </nav>
+     <Route exact path="/dersler">
+       <Dersler />
+     </Route>
+     <Route exact path="/profil">
+       <Profil />
+     </Route>
+   </Router>
+ );
+}
+
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
+import Dersler from './Dersler';
+import Profil from './Profil';
+
+
+function App() {
+ return (
+   <Router>
+     <nav>
+    
+     </nav>
+     <Route exact path="/dersler">
+       <Dersler />
+     </Route>
+     <Route exact path="/profil">
+       <Profil />
+     </Route>
+   </Router>
+ );
+}
+
+
+export default App;
+
+*/
