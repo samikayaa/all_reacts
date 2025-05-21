40b4afb358fc0fb108650386ff4d80aeec104ef4
samikayaa <samikaya89@gmail.com>
2025-05-21 21:46:14 +0300

    c

--- sprint7/gun2/q1/login.js
+++ sprint7/gun2/q1/login.js
@@ -1,6 +1,9 @@
 export default function Login() {
     return (<form>
       <h1>Register to Newsletter</h1>
+      <label>Email</label>
+      <button>Kayıt</button>
+      <input id="email" name="email" type="email"/>
     </form>)
   }
   
