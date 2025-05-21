aabacf1afa3e40927f48d5777ef073bd7ceddbce
samikayaa <samikaya89@gmail.com>
2025-05-21 22:13:44 +0300

    c

--- sprint7/gun2/q1/login.js
+++ sprint7/gun2/q1/login.js
@@ -1,9 +1,24 @@
+import { useState } from "react"
+
 export default function Login() {
-    return (<form>
+
+    const [email, setEmail] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(email)
+        setEmail("")
+    }
+
+    function handleChange(event) {
+        console.log(setEmail(event.target.value))
+    }
+
+    return (<form onSubmit={handleSubmit}>
       <h1>Register to Newsletter</h1>
-      <label>Email</label>
+      <label>Email: </label>
       <button>Kayıt</button>
-      <input id="email" name="email" type="email"/>
+      <input id="email" name="email" type="email" value={email} onChange={handleChange}/>
     </form>)
   }
   
