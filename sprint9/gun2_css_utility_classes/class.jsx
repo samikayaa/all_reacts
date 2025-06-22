abb1cb5d1f26d0bcf2565bea39339cf6fd517bfe
samikayaa <samikaya89@gmail.com>
2025-06-22 13:30:04 +0300

    c

--- sprint9/gun2_css_utility_classes/class.jsx
+++ sprint9/gun2_css_utility_classes/class.jsx
@@ -4,12 +4,29 @@ WORKINTECH sitesinin tasarımını inceledik. CSS açısından nasıl olduğuna 
 
 
 
+
 tailwindcss.com sayfasını detaylı inceledik. --> https://tailwindcss.com/docs/installation/using-vite 
+tailwind sayesinde html ve css kodlarını aynı dosyada kolayca yazabiliyoruz.
 
+
+1. aşama;
+FOTO
 kurulum : npm install -D tailwindcss
-initialize: npx tailwindcss init
+initialize: npx tailwindcss init --> bunu çalıştırdığımızda projemize "tailwind.config.js" dosyası gelecektir.
 
 
 
+2. aşama;
+FOTO
+
+
+
+3. aşama;
+FOTO
+
+
+
+4. aşama;
+eklentilere "Tailwind CSS Intellicense" ekleyip kod yazımı sırasında önerileri alabiliriz.
 
 */
