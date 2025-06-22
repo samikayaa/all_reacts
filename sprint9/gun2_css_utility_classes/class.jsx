4c11049781d81a577ad7b35a780fa94eaa4ca047
samikayaa <samikaya89@gmail.com>
2025-06-22 13:20:16 +0300

    c

---
+++ sprint9/gun2_css_utility_classes/class.jsx
@@ -0,0 +1,15 @@
+/*
+
+WORKINTECH sitesinin tasarımını inceledik. CSS açısından nasıl olduğuna bakarak inceledik.
+
+
+
+tailwindcss.com sayfasını detaylı inceledik. --> https://tailwindcss.com/docs/installation/using-vite 
+
+kurulum : npm install -D tailwindcss
+initialize: npx tailwindcss init
+
+
+
+
+*/
