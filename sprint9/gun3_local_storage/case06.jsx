52e14856df2480462b4fac25ede3328ab7557fc7
samikayaa <samikaya89@gmail.com>
2025-06-24 20:46:46 +0300

    c

---
+++ sprint9/gun3_local_storage/case06.jsx
@@ -0,0 +1,78 @@
+/*
+
+usePreference isimli bir custon hook tanımla ve App.js'deki state'lerde kullan. 
+- usePereference.js dosyasında named export ve arrow function olarak usePereference hook'unu yaz. 
+- usePreference hook'u bir key ve initialValue almalı. 
+- useLocalStorage hook'u ile value isimli bir state tanımlamalı. 
+- İçinde updatePreference isimli bir fonksiyon olmalı. newValue parametresi ile gelen değeri state'e set etmeli. 
+- Bu hook array tipinde value ve updatePreference'ı return etmeli. 
+- Hook'u yazdıktan sonra App.js'deki state'leri bu hook ile değiştirmeyi unutma.
+
+*/
+
+
+//App.js
+
+import { useState } from 'react';
+
+export default function App() {
+    const [theme, setTheme] = usePreference("light") //useState("light");
+    const [language, setLanguage] = usePreference("tr")  //useState("tr");
+
+    return (
+        <div>
+            <label>
+                Theme:
+                <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+                    <option value="light">Light</option>
+                    <option value="dark">Dark</option>
+                </select>
+            </label>
+            <label>
+                Language:
+                <select value={language} onChange={(e) => setLanguage(e.target.value)}>
+                    <option value="en">English</option>
+                    <option value="tr">Turkish</option>
+                </select>
+            </label>
+        </div>
+    )
+}
+
+
+
+
+//usePreference.js
+
+import { useLocalStorage } from './useLocalStorage.js'
+
+export const usePreference = (key, initialValue) => {
+    const [value, setValue] = useLocalStorage(key, initialValue);
+
+    const updatePreference = (newValue) => {
+        setValue(newValue);
+    }
+
+    return [value, updatePreference]
+
+}
+
+
+
+//useLocalStorage.js
+
+import { useState, useEffect } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+    const [value, setValue] = useState(() => {
+        const storedValue = localStorage.getItem(key);
+        return storedValue ? JSON.parse(storedValue) : initialValue;
+    });
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+
+    return [value, setValue];
+};
+
