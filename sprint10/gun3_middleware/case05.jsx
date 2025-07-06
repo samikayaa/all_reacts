579c7d1b41e7f2c00052150961f54e7a6aab1b9f
samikayaa <samikaya89@gmail.com>
2025-07-06 21:41:03 +0300

    c

--- sprint10/gun3_middleware/case05.jsx
+++ sprint10/gun3_middleware/case05.jsx
@@ -1,3 +1,33 @@
 /*
+Aşağıdaki thunk action'ı tamamla. 
+
+1- fetchData action'ı ilk olarak state'i loading'e almalı. 
+- başlangıçta FETCH_DATA_LOADING tipinde action dispatch et. 
+
+2- axios ile https://api.example.com/data adresinden data çekmeli. 
+- success aldığında FETCH_DATA_SUCCESS tipinde bir action dispatch etmeli. 
+payload olarak response'da gelen data'yı vermeli. 
+- hata aldığında FETCH_DATA_ERROR tipinde bir action dispatch etmeli. 
+payload olarak error objesindeki message'ı vermeli.
 
 */
+
+
+import axios from "axios";
+
+export const FETCH_DATA_LOADING = "Fetching is in progress";
+export const FETCH_DATA_SUCCESS = "Fetch job succeeded";
+export const FETCH_DATA_ERROR = "Fetch job got error";
+
+
+export const fetchData = () => (dispatch) => {
+    dispatch({ type: 'FETCH_DATA_LOADING' });
+    axios.get('https://api.example.com/data')
+        .then((response) => {
+            dispatch({ type: 'FETCH_DATA_SUCCESS', payload: response.data });
+        })
+        .catch((error) => {
+            dispatch({ type: 'FETCH_DATA_ERROR', payload: error.message });
+        })
+};
+
---
+++ sprint10/gun4_TanStackQuery/class.jsx
@@ -0,0 +1,36 @@
+/*
+Aşağıdaki useQuery hook'unda /api/todos adresine axios isteği(get) at. 
+- Gelen cevabı response isimli bir değişkende sakla. 
+- await kullanarak tanımla. 
+- response içindeki data'yı dön. 
+Not: await için async fonksiyon tanımlamalısın.
+
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const {data, error, isLoading} = useQuery({
+ queryKey: ['todos'],
+ queryFn: async() => {
+   const response = await axios.get("/api/todos");
+   return response.data;
+ }
+});
+*/
+
+
+
+/*
+Aşağıdaki query'nin 10sn boyunca bayatlamamasını sağla. 
+- İpucu: staleTime
+
+
+useQuery({
+ queryKey: ['todos'],
+ queryFn: async () => {
+   const response = await fetch('/api/todos');
+   return response.json();
+ },
+ staleTime: 10000
+});
+
+*/
