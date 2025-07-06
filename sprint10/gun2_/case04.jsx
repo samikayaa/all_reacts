bfa9309c623d3caa8d12207594fa094c63a43ad2
samikayaa <samikaya89@gmail.com>
2025-07-06 19:45:41 +0300

    c

---
+++ sprint10/gun2_/case04.jsx
@@ -0,0 +1,56 @@
+/*
+Aşağıda tanımlanmış olan reducer'ları redıx store'da kullanmak için birleştir. 
+- birleştirilmiş reducer'un ismi reducer olsun. 
+- userReducer'ı user, postsReducer'ı posts key'ini kullan. 
+- named export olsun.
+*/
+
+import { combineReducers } from "redux"; //bunu yazdım.
+
+const userReducer = (state = {}, action) => {
+    switch (action.type) {
+        case 'SET_USER':
+            return { ...state, user: action.payload };
+        default:
+            return state;
+    }
+};
+
+const postsReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_POST':
+            return [...state, action.payload];
+        default:
+            return state;
+    }
+};
+
+export const reducer = combineReducers({
+    user: userReducer,
+    posts: postsReducer,
+});
+
+/*
+const userReducer = (state = {}, action) => {
+    switch (action.type) {
+      case 'SET_USER':
+        return { ...state, user: action.payload };
+      default:
+        return state;
+    }
+  };
+  
+  const postsReducer = (state = [], action) => {
+    switch (action.type) {
+      case 'ADD_POST':
+        return [...state, action.payload];
+      default:
+        return state;
+    }
+  };
+  
+  export const reducer = combineReducers({
+    user: userReducer,
+    posts: postsReducer,
+  });
+  */
