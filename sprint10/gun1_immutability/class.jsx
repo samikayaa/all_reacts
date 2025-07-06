e2be4e54b9a933fc987c3df4393c7689332d5cd0
samikayaa <samikaya89@gmail.com>
2025-07-06 12:19:08 +0300

    c

---
+++ sprint10/gun1_immutability/class.jsx
@@ -0,0 +1,18 @@
+/*
+
+immutability
+
+*/
+
+
+
+
+/*
+
+Reducer, bir fonksiyondur.
+Şu şekilde çalışır:
+(state, action) => newState
+Yani bir önceki state ve gelen action’a göre yeni bir state döner. 
+Bu fonksiyonun temel özelliği: pure function (saf fonksiyon) olmasıdır — dış etkenlerden bağımsızdır.
+
+*/
