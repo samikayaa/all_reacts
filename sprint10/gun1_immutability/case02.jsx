eb7dd3f27ade802776bc8840fa04cc664355f4b3
samikayaa <samikaya89@gmail.com>
2025-07-06 12:27:23 +0300

    c

--- sprint10/gun1_immutability/case02.jsx
+++ sprint10/gun1_immutability/case02.jsx
@@ -1,3 +1,18 @@
 /*
 
+ Aşağıdaki ogrenci objesini immutable bir şekilde güncelleyerek, 
+ 
+ yeniOgrenci adında yeni bir obje oluştur.
+ İsim özelliğini Ahmet Yılmaz olarak değiştir.
+
 */
+
+
+const ogrenci = {
+    id: 1,
+    isim: "Ayşe Demir",
+    yas: 20,
+};
+
+
+const yeniOgrenci = { ...ogrenci, isim: "Ahmet Yılmaz" }; //burayı yazdık.
