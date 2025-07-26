a3d61e8d4db4d0a913f33f41294ef60b57ad136a
samikayaa <samikaya89@gmail.com>
2025-07-26 17:56:46 +0300

    c

--- sprint11/gun2_token/case05.jsx
+++ sprint11/gun2_token/case05.jsx
@@ -10,6 +10,5 @@ import jwtDecode from "jwt-decode";
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcklkIjoiMzAwMTUxIiwibmFtZSI6IkZhdGloIFnEsWxkxLF6IiwiaWF0IjoxNzM3MjM5MDIyfQ.qwCfhgzJ_XuCPxEyGtt0whDsZ-zIycL2ZvMi5-gWZW4";
 
-
 const decoded = jwtDecode(token);
 console.log(decoded.userId);
